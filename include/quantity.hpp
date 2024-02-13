19a054808bc38f4cd0388c59e10ccb61b6d83da4
rixfxol <54469435+rixfxol@users.noreply.github.com>
2024-02-14 00:06:25 +0800

    initialized working tree

---
+++ include/quantity.hpp
@@ -0,0 +1,10 @@
+/**
+ * @file quantity.hpp
+ * @author rixfxol (jf_espiritu.fa@outlook.ph)
+ * @brief The declaration file for 
+ * @version 0.1
+ * @date 2024-02-13
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
